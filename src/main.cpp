a105bfccc40962a6b92fb6a1e5493a66705a44c5
TomD53 <tomtomgaming101@gmail.com>
2021-09-09 22:04:49 +0100

    use raw data, add packet loss

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,21 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-struct Payload
+struct Payload // MAX 32 BYTES
 {
-  float yaw;
-  float pitch;
-  float roll;
+  // 12 bytes of MPU data
+  int16_t ax, ay, az;  // acceleration values in all 3 axis
+  int16_t gx, gy, gz;  // gyroscope acceleration values in all 3 axis
 
-  short aquart_x;
-  short aquart_y;
-  short aquart_z;
 
-  long pressure;
-  int altitude;
-  float temp;
+  long pressure; // 4 bytes
+  int altitude;  // 2 bytes
+  float temp;  // 4 bytes
 
-  bool digital_channel_1;
-  bool digital_channel_2;
+  unsigned int vcc;  // 2 bytes
+  unsigned int packet_id;  // 2 bytes
+
+  // we have 26 bytes total
 };
 
 Payload payload;
@@ -32,10 +31,17 @@ void setup()
 {
   Serial.begin(115200);
   radio.begin();
-  radio.setDataRate(RF24_1MBPS);
+  radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
   radio.openReadingPipe(1, pipe);
 }
+
+unsigned int last_packet_id;
+long last_recv_id;
+long recv_id;
+long timedelta;
+long last_start_time = micros();
+int loss_percent;
 
 void loop()
 {
@@ -44,24 +50,64 @@ void loop()
   {
     radio.read(&payload, sizeof(Payload));
 
-    Serial.print(payload.yaw);
-    Serial.print(",");
-    Serial.print(payload.pitch);
-    Serial.print(",");
-    Serial.print(payload.roll);
-    Serial.print(",");
-    Serial.print(payload.aquart_x);
-    Serial.print(",");
-    Serial.print(payload.aquart_y);
-    Serial.print(",");
-    Serial.print(payload.aquart_z);
-    Serial.print(",");
-    Serial.print(payload.pressure);
-    Serial.print(",");
-    Serial.print(payload.altitude);
-    Serial.print(",");
-    Serial.print(payload.temp);
+    ++recv_id;
+  }
 
-    Serial.println();
+  Serial.print(payload.ax);
+  Serial.print(",");
+  Serial.print(payload.ay);
+  Serial.print(",");
+  Serial.print(payload.az);
+  Serial.print(",");
+  Serial.print(payload.gx);
+  Serial.print(",");
+  Serial.print(payload.gy);
+  Serial.print(",");
+  Serial.print(payload.gz);
+  Serial.print(",");
+  Serial.print(payload.pressure);
+  Serial.print(",");
+  Serial.print(payload.altitude);
+  Serial.print(",");
+  Serial.print(payload.temp);
+  Serial.print(",");
+  Serial.print(payload.vcc);
+  Serial.print(",");
+  Serial.print(payload.packet_id);
+  Serial.print(",");
+  Serial.print(loss_percent);
+
+  Serial.println();
+
+
+  timedelta = micros() - last_start_time;
+  if (timedelta >= 1000000) {
+    if (payload.packet_id >= last_packet_id) {
+      
+      unsigned int packet_delta = payload.packet_id - last_packet_id;
+      //Serial.print("packet_delta: ");
+      //Serial.print(packet_delta);
+      unsigned int recv_delta = recv_id - last_recv_id;
+      //Serial.print(" | recv_delta: ");
+      //Serial.print(recv_delta);
+
+      // if there was some packet loss, packet_delta will be higher than recv_delta and
+      // there will be some difference `diff`
+
+      int diff = packet_delta - recv_delta;
+      //Serial.print(" | diff: ");
+      //Serial.print(diff);
+      if (packet_delta == 0 && recv_delta == 0) {
+        loss_percent = 100;
+      } else {
+        loss_percent = (diff * 100) / packet_delta;
+      }
+      //Serial.print(" | loss_percent: ");
+      //Serial.println(loss_percent);
+    }
+
+    last_recv_id = recv_id;
+    last_start_time = micros();
+    last_packet_id = payload.packet_id;
   }
 }
